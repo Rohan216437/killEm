967d922a7a6b9a0c5a6d4cf8342e6c55cbdeeeef
Rohan Gupta <7039426154rg@gmail.com>
2024-05-05 21:58:06 +0530

    play ground page

---
+++ src/components/PlayGround.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const PlayGround = () => {
+    const [count, setCount] = useState(0);
+    const [buttonPosition, setButtonPosition] = useState({ top: 28, left: 47 });
+    const [timerId, setTimerId] = useState(null);
+    const navigate = useNavigate();
+
+    const incrementCount = () => {
+        setCount(count + 1);
+        const randomTop = Math.random() * 75; 
+        const randomLeft = Math.random() * 75;
+        setButtonPosition({ top: randomTop, left: randomLeft });
+        clearTimeout(timerId);
+    };
+
+    const handleBackClick = () => {
+        navigate('/');
+        window.location.reload();
+    };
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            const randomTop = Math.random() * 75; 
+            const randomLeft = Math.random() * 75;
+            setButtonPosition({ top: randomTop, left: randomLeft });
+            setCount(count - 1)
+        }, 5000);
+
+        setTimerId(timeoutId);
+
+        return () => clearTimeout(timeoutId);
+    }, [buttonPosition , count]);
+
+    const buttonStyle = {
+        top: `${buttonPosition.top}%`,
+        left: `${buttonPosition.left}%`,
+        // transition: 'top 0.5s, left 0.5s',
+    };
+ 
+  return (
+    <div className='p-8 flex-col h-screen w-screen'>
+        <div>
+            <button onClick={handleBackClick}>Back</button>
+        </div>
+        <div className='flex justify-center items-center h-1/4'>
+            <div className='flex'>
+                <div className=''>
+
+                </div>
+                <h2 className=''>
+                    {count}
+                </h2>
+                <h2 className=''>
+                    5
+                </h2>
+            </div>
+        </div>
+        <div className='flex justify-center items-center h-3/4 relative'>
+            <button style={buttonStyle} className="absolute w-20 h-20 bg-green-600" onClick={incrementCount}>
+            Increment
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default PlayGround;
