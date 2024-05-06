ea1982987a30b5820c4c967c2e0b8fd09ddf32dc
Rohan Gupta <7039426154rg@gmail.com>
2024-05-06 22:39:37 +0530

    miss chance , game over logic added

--- src/components/PlayGround.jsx
+++ src/components/PlayGround.jsx
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom';
 const PlayGround = () => {
     const [count, setCount] = useState(0);
     const [buttonPosition, setButtonPosition] = useState({ top: 28, left: 47 });
+    const [chances, setChances] = useState(2);
     const [timerId, setTimerId] = useState(null);
+    const [gameOver, setGameOver] = useState(false);
     const navigate = useNavigate();
 
     const incrementCount = () => {
@@ -25,42 +27,48 @@ const PlayGround = () => {
             const randomTop = Math.random() * 75; 
             const randomLeft = Math.random() * 75;
             setButtonPosition({ top: randomTop, left: randomLeft });
-            setCount(count - 1)
+            setChances(chances - 1);
         }, 5000);
 
         setTimerId(timeoutId);
 
         return () => clearTimeout(timeoutId);
-    }, [buttonPosition , count]);
+    }, [buttonPosition, count, chances]);
+
+    useEffect(() => {
+        if (chances === 0) {
+            setGameOver(true);
+            clearTimeout(timerId);
+        }
+    }, [chances, timerId]);
 
     const buttonStyle = {
         top: `${buttonPosition.top}%`,
         left: `${buttonPosition.left}%`,
-        // transition: 'top 0.5s, left 0.5s',
     };
- 
-  return (
-    <div className='p-8 flex-col h-screen w-screen'>
-        <div>
-            <button onClick={handleBackClick}>Back</button>
+
+    return (
+        <div className='p-8 flex-col h-screen w-screen'>
+            <div>
+                <button onClick={handleBackClick}>Back</button>
+            </div>
+            <div className='flex justify-between items-center h-1/4'>
+                <div className='w-20 h-1/4'></div>
+                    <h2 className='w-20 flex justify-center items-center'>{count}</h2>
+                <h2>Chances: {chances}</h2>
+            </div>
+
+            <div className='flex justify-center items-center h-3/4 relative'>
+                {gameOver ? (
+                    <h2>Game Over</h2>
+                ) : (
+                    <button style={buttonStyle} className="absolute w-20 h-20 bg-green-600" onClick={incrementCount}>
+                        Increment
+                    </button>
+                )}
+            </div>
         </div>
-        <div className='flex justify-between items-center h-1/4'>
-            <div></div>
-    <div className='flex items-center'>
-        <h2>{count}</h2>
-    </div>
-    <h2>Chances: 5</h2>
-</div>
-
-
-
-        <div className='flex justify-center items-center h-3/4 relative'>
-            <button style={buttonStyle} className="absolute w-20 h-20 bg-green-600" onClick={incrementCount}>
-            Increment
-            </button>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default PlayGround;
