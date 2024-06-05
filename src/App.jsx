31cee4863497ee4f8775b3db9439b9138a48376f
Rohan Gupta <7039426154rg@gmail.com>
2024-06-05 22:57:34 +0530

    App,jsx changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PlayGround from './components/PlayGround';
-import PlayCoverNoBG from './assets/PlayCoverNoBG.png'
+import PlayCoverNoBG from './assets/PlayCoverPageMole.png';
 
 function App() {
   const [playClicked, setPlayClicked] = useState(false);
@@ -11,10 +11,17 @@ function App() {
       <div className='bg-sky-500 h-screen'>
         {!playClicked && (
           <div className='flex justify-center h-full w-screen'>
-            <div className="bg-sky-500 flex flex-col items-center">
-              <img src={PlayCoverNoBG} alt="Cover PNG here"  className="w-full h-auto" style={{ backgroundColor: 'transparent' }}/>
+            <div className="bg-sky-500 flex flex-col items-center relative">
+              <div className="relative">
+                <img src={PlayCoverNoBG} alt="Cover PNG here" className="w-full h-auto" style={{ backgroundColor: 'transparent' }}/>
+                <svg className='absolute bottom-0 left-1/2 transform -translate-x-1/2' width="100%" height="130" viewBox="0 0 500 100">
+                  {/* <path id="curve" d="M 50,90 Q 250,10 450,90" fill="transparent"/> */}
+                  <path id="curve" d="M 50,50 Q 230,140 450,50" fill="transparent"/>
+                  
+                </svg>
+              </div>
               <Link to="/playground" className='mt-5'>
-                <button onClick={() => setPlayClicked(true)} className=" bg-white hover:bg-sky-100 text-sky-500 font-bold py-6 px-10 rounded-full shadow-md hover:shadow-2xl transition duration-500">
+                <button onClick={() => setPlayClicked(true)} className="bg-white hover:bg-sky-100 text-sky-500 font-bold py-6 px-10 rounded-full shadow-md hover:shadow-2xl transition duration-500">
                   Start Game
                 </button>
               </Link>
