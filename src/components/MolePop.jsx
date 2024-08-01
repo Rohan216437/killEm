11a0bccc7cc8d6a9d7d8bec50c31062b5ec30e95
Rohan Gupta <7039426154rg@gmail.com>
2024-08-01 22:35:33 +0530

    molepop component changes

--- src/components/MolePop.jsx
+++ src/components/MolePop.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Dirt from '../assets/dirt.png';
 import Mole from '../assets/mole1.png';
+// import '../index.css';
 
 const MolePop = () => {
   const [isPopped, setIsPopped] = useState(false);
@@ -10,19 +10,9 @@ const MolePop = () => {
   }, []);
 
   return (
-    <div className="relative w-48 h-48 overflow-hidden">
-      <div className={`absolute w-full h-full bottom-0 flex justify-center items-end ${isPopped ? 'animate-pop-up' : ''}`}>
-        <img src={Mole} alt="mole" className="w-24 h-24" />
-      </div>
-      <div
-        className="absolute w-full h-1/3 bottom-0"
-        style={{
-          backgroundImage: `url(${Dirt})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'top',
-        }}
-      ></div>
+    <div className="mole-container">
+      
+      <div className="dirt"></div>
     </div>
   );
 };
